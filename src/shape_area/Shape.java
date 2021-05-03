2c5e0867d3bf5d90deecc715b08da3df0ecc0081
theo-jin <83561523+theo-jin@users.noreply.github.com>
2021-05-03 17:20:38 +0900

    shape.area

---
+++ src/shape_area/Shape.java
@@ -0,0 +1,10 @@
+package shape_area;
+
+public interface Shape {
+	
+	public void setData(int value1, int value2);
+	
+	public double calcArea();
+}
+
+
---
+++ src/shape_area/ShapeType.java
@@ -0,0 +1,6 @@
+package shape_area;
+
+public enum ShapeType {
+	삼각형,
+	사각형
+}
